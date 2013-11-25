a51c0c7f4306e0554815ea182afd13a5d5989d34
Pradeep <chaudharipradeep9@gmail.com>
2013-11-25 04:37:50 -0700

    using gson for parsing

---
+++ nlpBitBucket/src/com/asu/nlp/model/SentimentData.java
@@ -0,0 +1,39 @@
+package com.asu.nlp.model;
+public class SentimentData{
+		public SentimentData(String bullish, String bearish, String timestamp,
+			String stock) {
+		super();
+		this.bullish = bullish;
+		this.bearish = bearish;
+		this.timestamp = timestamp;
+		this.stock = stock;
+	}
+		String bullish;
+		public String getBullish() {
+			return bullish;
+		}
+		public void setBullish(String bullish) {
+			this.bullish = bullish;
+		}
+		public String getBearish() {
+			return bearish;
+		}
+		public void setBearish(String bearish) {
+			this.bearish = bearish;
+		}
+		public String getTimestamp() {
+			return timestamp;
+		}
+		public void setTimestamp(String timestamp) {
+			this.timestamp = timestamp;
+		}
+		public String getStock() {
+			return stock;
+		}
+		public void setStock(String stock) {
+			this.stock = stock;
+		}
+		String bearish;
+		String timestamp;
+		String stock;
+	}
---
+++ nlpBitBucket/src/com/asu/nlp/model/VolumeData.java
@@ -0,0 +1,41 @@
+package com.asu.nlp.model;
+public class VolumeData{
+		public VolumeData(String timestamp, String volume_score,
+				String volume_change,String stockSymbol) {
+			super();
+			this.timestamp = timestamp;
+			this.volume_score = volume_score;
+			this.volume_change = volume_change;
+			this.stockSymbol=stockSymbol;
+		}
+		String timestamp;
+		private String stockSymbol;
+		public String getTimestamp() {
+			return timestamp;
+		}
+		public void setTimestamp(String timestamp) {
+			this.timestamp = timestamp;
+		}
+		public String getVolume_score() {
+			return volume_score;
+		}
+		public void setVolume_score(String volume_score) {
+			this.volume_score = volume_score;
+		}
+		public String getVolume_change() {
+			return volume_change;
+		}
+		public void setVolume_change(String volume_change) {
+			this.volume_change = volume_change;
+		}
+		public String getStockSymbol() {
+			return stockSymbol;
+		}
+		public void setStockSymbol(String stockSymbol) {
+			this.stockSymbol = stockSymbol;
+		}
+		String volume_score;
+		String volume_change;
+		
+		
+	}
